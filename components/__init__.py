bbd3ea7e701515dd2805b8769e95455bb0727253
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-03-04 00:58:25 +0000

    Simpler names for Head, Hand components

--- components/__init__.py
+++ components/__init__.py
@@ -2,7 +2,7 @@
 from luna_rig.components.stretch_component import IKSplineStretch
 from luna_rig.components.character_component import Character
 from luna_rig.components.fk_component import FKComponent
-from luna_rig.components.fk_component import HeadComponent
+from luna_rig.components.fk_component import Head
 from luna_rig.components.ik_component import IKComponent
 from luna_rig.components.fkik_component import FKIKComponent
 from luna_rig.components.fk_dynamics_component import FKDynamicsComponent
@@ -10,7 +10,7 @@ from luna_rig.components.spine_component import FKIKSpine
 from luna_rig.components.spine_component import RibbonSpine
 from luna_rig.components.foot_component import FootComponent
 from luna_rig.components.twist_component import TwistComponent
-from luna_rig.components.hand_component import HandComponent
+from luna_rig.components.hand_component import Hand
 from luna_rig.components.eye_component import EyeComponent
 from luna_rig.components.empty_component import EmptyComponent
 
