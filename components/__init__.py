af8b17f327d534550509cf257f649ed337572e21
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-03-04 01:00:32 +0000

    Simpler Foot class name

--- components/__init__.py
+++ components/__init__.py
@@ -8,7 +8,7 @@ from luna_rig.components.fkik_component import FKIKComponent
 from luna_rig.components.fk_dynamics_component import FKDynamics
 from luna_rig.components.spine_component import FKIKSpine
 from luna_rig.components.spine_component import RibbonSpine
-from luna_rig.components.foot_component import FootComponent
+from luna_rig.components.foot_component import Foot
 from luna_rig.components.twist_component import TwistComponent
 from luna_rig.components.hand_component import Hand
 from luna_rig.components.eye_component import EyeComponent
--- components/foot_component.py
+++ components/foot_component.py
@@ -6,7 +6,7 @@ from luna_rig.functions import attrFn
 from luna_rig.functions import nameFn
 
 
-class FootComponent(luna_rig.AnimComponent):
+class Foot(luna_rig.AnimComponent):
     ROLL_ATTRS = ["footRoll", "toeRoll", "heelRoll", "bank", "heelTwist", "toeTwist", "toeTap"]
 
     @property
@@ -31,7 +31,7 @@ class FootComponent(luna_rig.AnimComponent):
         side = side if side else meta_parent.side
         foot_locators_grp = pm.PyNode(foot_locators_grp)  # type: luna_rig.nt.Transform
         # Create instance and add attrs
-        instance = super(FootComponent, cls).create(meta_parent=meta_parent, side=side, name=name, character=character)  # type: FootComponent
+        instance = super(Foot, cls).create(meta_parent=meta_parent, side=side, name=name, character=character)  # type: Foot
         instance.pynode.addAttr("fkChain", at="message", multi=1, im=0)
         instance.pynode.addAttr("ikChain", at="message", multi=1, im=0)
         instance.pynode.addAttr("fkControl", at="message")
@@ -97,7 +97,7 @@ class FootComponent(luna_rig.AnimComponent):
 
         # Roll attributes
         attrFn.add_divider(meta_parent.ik_control.transform, attr_name="FOOT")
-        for attr_name in FootComponent.ROLL_ATTRS:
+        for attr_name in Foot.ROLL_ATTRS:
             meta_parent.ik_control.transform.addAttr(attr_name, at="float", k=1, dv=0.0)
         meta_parent.ik_control.transform.footRoll.connect(rv_chain[2].attr(roll_axis))
         meta_parent.ik_control.transform.toeRoll.connect(toe_locator.rotateX)
@@ -148,4 +148,4 @@ class FootComponent(luna_rig.AnimComponent):
         pm.deleteAttr(self.meta_parent.ik_control.transform.FOOT)
         for attr_name in self.ROLL_ATTRS:
             pm.deleteAttr(self.meta_parent.ik_control.transform.attr(attr_name))
-        super(FootComponent, self).remove()
+        super(Foot, self).remove()
