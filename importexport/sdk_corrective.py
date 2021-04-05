e262094c54c4c4507c9c990011d2943e104fabda
S0nic014 <47799401+S0nic014@users.noreply.github.com>
2021-04-05 06:36:55 +0100

    Fixes for sdk_corrective manager

--- importexport/sdk_corrective.py
+++ importexport/sdk_corrective.py
@@ -24,30 +24,33 @@ class SDKCorrectiveManager(manager.AbstractManager):
     def get_latest_file(self, node_name, pose_name):
         return fileFn.get_latest_file(self.get_base_name(node_name, pose_name), self.path, extension=self.extension, full_path=True)
 
-    def export_pose(self, corrective_component, pose_name, driver):
+    def export_pose(self, corrective_component, pose_name, driver, control_filter_list=[]):
         pose_dict = corrective_component.get_pose_dict()  # type: dict
         export_path = self.get_new_file(corrective_component.pynode.name(), pose_name)
         for corr_control in pose_dict.keys():
-            pose_dict[corr_control]["driver"] = driver
-            pose_dict[corr_control]["driver_value"] = pm.getAttr(pose_dict[corr_control]["driver"])
-            fileFn.write_json(export_path, data=pose_dict, sort_keys=False)
-            Logger.info("{0}: Exported {1} pose {2}".format(self, corrective_component, export_path))
+            if control_filter_list and corr_control not in control_filter_list:
+                pose_dict.pop(corr_control)
+                continue
+        pose_dict["driver"] = driver
+        pose_dict["driver_value"] = pm.getAttr(driver)
+        fileFn.write_json(export_path, data=pose_dict, sort_keys=False)
+        Logger.info("{0}: Exported {1} pose {2}".format(self, corrective_component, export_path))
 
     def import_pose(self, corrective_component, pose_name):
         if isinstance(corrective_component, luna_rig.AnimComponent):
             corrective_component = corrective_component.pynode.name()
         latest_file = self.get_latest_file(corrective_component, pose_name)
-        pose_dict = fileFn.load_json(latest_file)  # type: dict
+        pose_dict = fileFn.load_json(latest_file)  # type: dict#
+        driver_attr = pose_dict.pop("driver")
+        driver_value = pose_dict.pop("driver_value", 0.0)
+        if not pm.objExists(driver_attr):
+            Logger.warning("{0}: Driver attr {1} no longer exists".format(self, driver_attr))
+            return
+
         for corr_control_name in pose_dict.keys():
             if not pm.objExists(corr_control_name):
                 Logger.warning("{0}: Control {1} no longer exists, skipping...".format(self, corr_control_name))
                 continue
-            driver_attr = pose_dict[corr_control_name].pop("driver")
-            driver_value = pose_dict[corr_control_name].pop("driver_value", 0.0)
-            if not pm.objExists(driver_attr):
-                Logger.warning("{0}: Driver attr {1} no longer exists".format(self, driver_attr))
-                continue
-
             control = luna_rig.Control(corr_control_name)
             for attr, value_list in pose_dict[corr_control_name].items():
                 axis_dict = {attr + "X": value_list[0],
